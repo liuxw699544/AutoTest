63586a8a37a28b8d94be4b4453a85d9565409a5a
liuxw <liuxw@jccfc.com>
2021-04-14 16:56:03 +0800

    超时测试失败案例

--- Chapter5/src/main/java/com/course/testng/TimeOutTest.java
+++ Chapter5/src/main/java/com/course/testng/TimeOutTest.java
@@ -8,4 +8,8 @@ public class TimeOutTest {
         Thread.sleep(2000);
 
     }
+    @Test(timeOut = 2000)//设置睡眠3秒，但是2秒时候未返回结果，所以就是失败了
+    public void testFailed() throws InterruptedException{
+        Thread.sleep(3000);
+    }
 }
